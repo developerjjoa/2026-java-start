99fa66bf25baf725bcb80b67a4ff4cb7214d2006
developerjjoa <developerjjoa@gmail.com>
2026-01-04 23:11:16 +0900

    feat: 8. 훈련 - 문제와 풀이2(변수 값 교환) - 변수의 값을 서로 바꿀 때는 한번에 서로 교환할 수가 없다. a = b라고 하면 a의 값이 사라지기 때문이다. - 변수의 값을 서로 바꿀 때는 temp 변수를 활용해야 한다.

---
+++ src/scanner/ex/changeVarEx.java
@@ -0,0 +1,18 @@
+package scanner.ex;
+
+public class changeVarEx {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        //시작: 코드를 작성하세요
+        int temp = a;
+        a = b;
+        b = temp;
+        //종료: 코드를 작성하세요
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
